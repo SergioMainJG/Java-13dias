fac4cdcc56d8a15712d40e7af95142b8384ee149
Sergio Arce <126024056+SergioMainJG@users.noreply.github.com>
2023-07-12 19:56:18 -0600

    Quiero volver a dominar Java aaa
    asies, main Java desde la beta

---
+++ 01-ReAprender-Java/src/clases/Persona.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class Persona {
+    
+    String nombre;
+    String apellido;
+    
+    public void desplegarInformacion(){
+        
+        System.out.println("Nombre: " + nombre + "\n"+ "Apellido:" + apellido);
+    }
+    
+}
---
+++ 01-ReAprender-Java/src/clases/PruebaPersona.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class PruebaPersona {
+
+    public static void main(String[] args){
+     
+        Persona persona1= new Persona();
+        
+        persona1.nombre = "Juan";
+        persona1.apellido = "Cucknizo";
+        
+        persona1.desplegarInformacion();
+
+    }
+    
+}
